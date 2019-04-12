4396d6c60c86c53da6a987d60e845d3a35da78d2
Md. Ariful Islam, 170042040 <arifulislam40@iut-dhaka.edu>
2019-04-12 21:02:30 +0600

    completed

---
+++ Edu/Education/app/src/main/java/com/example/education/registerTeacher.java
@@ -0,0 +1,13 @@
+package com.example.education;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registerTeacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_teacher);
+    }
+}
